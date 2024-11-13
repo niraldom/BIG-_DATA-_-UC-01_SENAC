f634f5bf83c832592d3369d88e0233a9ddfe9178
niraldom <niraldom@hotmail.com>
2024-11-13 18:42:41 -0300

    Add files via upload

---
+++ aula_01_01.py
@@ -0,0 +1,5 @@
+n1 = 10
+n2 = 5.6
+nome = "Niraldo"
+idade = 47
+print("Sr(a)", nome, ", a sua idade é", idade)
---
+++ aula_01_02.py
@@ -0,0 +1,3 @@
+num = int(input("informe um valor: "))
+result = num * 2
+print("o resultado do dobro do número é: ", result)
