a5822c5cd86868db2bfec384425fcb8ecfed4f86
niraldom <niraldom@hotmail.com>
2024-11-25 14:10:29 -0300

    Add files via upload
    aula 03 praticando

---
+++ aula_03_05.py
@@ -0,0 +1,8 @@
+# Programa doador de sangue
+idade = int(input("informe a sua idade: "))
+peso = float(input("informe o seu peso: "))
+sono = float(input("informe quantas horas dormiu na última noite:"))
+if (idade>=16) and (peso > 50) and (sono > 6)
+    print(f"Parabéns, você pode doar sangue hoje")
+else:
+    print(f"Infelizmente você não poderá doar sangue hoje")
