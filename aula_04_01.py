1e1aa9f7197ff9e79a416dbf5329af72d015b61e
niraldom <niraldom@hotmail.com>
2025-01-08 18:11:50 -0300

    Add files via upload

--- aula_04_01.py
+++ aula_04_01.py
@@ -1,15 +1,11 @@
-# estruturas de repetição
-for i in range (5):
-    print (i)
-# programa para exibir conteúdo de uma lista
-lista = [1, 2, 3, 4, 5]
-for item in lista:
-    print(item)
-# print (usando o comando while)
-i = 0
-while i<=4:
-    print(i)
-contador = 0
-while contador < 10:
-    print(f"valor do contador é {contador}")
-    contador += 1
+# Importando a Biblioteca
+import pandas as pd
+
+# importando a Base de Dados
+endereco_dados = 'BASES\Finaceira.csv'
+
+# Criando o DataFrame Financeira
+df_financeira = pd.read_csv(endereco_dados,sep=',',encoding='iso-8859-1')
+
+# Exibindo o DataFrame
+print(df_financeira.head())
--- aula_04_02.py
+++ aula_04_02.py
@@ -1,7 +1,18 @@
-# estrutura de repetição infinita
-i = 0
-resp = "S"
-while resp == "S"or resp == "s":
-    print(i)
-    i += 1
-    resp = input("deseja continuar (S/N)? ")
+# importando a Biblioteca Pandas
+import pandas as pd
+
+# Importando a Base de Dados
+endereco_dados = 'https://www.ispdados.rj.gov.br/Arquivos/BaseDPEvolucaoMensalCisp.csv'
+
+# Criando o DataFrame Ocorrências
+df_ocorrencias = pd.read_csv(endereco_dados,sep=';',encoding='iso-8859-1')
+
+# Criando o DataFrame Homicídio Doloso por Município
+df_hom_doloso_munic = df_ocorrencias[['munic','hom_doloso']]
+
+# Consolidando o DataFrame HOmicídio Doloso por Município
+df_hom_doloso_munic = df_hom_doloso_munic.groupby(['munic']).sum(['hom_doloso']).reset_index()
+
+
+# Exibindo o DataFrame
+print(df_ocorrencias.head())
